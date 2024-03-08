5663d9d215533d4da750b1aa096ebc09fd9ffeca
sherzodboy <kholmamatovsh09@gmail.com>
2024-03-08 15:47:47 +0500

    scope, hoisting, TDZ

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// function calcAge(birthYear) {
+//   function printAge() {
+//     const output = `You are ${age}, born in ${birthYear}`;
+//     console.log(output);
+//     return output;
+//   }
+//   const age = 2024 - birthYear;
+//   printAge();
+//   return age;
+// }
+
+// calcAge(2000);
+
+// -----------------------     hoisting
+// console.log(age);
+// console.log(yaer);
+// console.log(date);
+
+// let age = 20;
+// const yaer = 2024;
+// var date = 7;
+
+// ----------------------    tempral dead zone
+// if (true) {
+//   // TDZ start
+//   console.log(x);
+//   alert(1);
+
+//   let x = 10;
+//   // TDZ end
+// }
